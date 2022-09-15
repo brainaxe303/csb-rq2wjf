2e601fe9c165a24eeefddb22a6d036e1b56b5435
Code Sandbox <hello@codesandbox.io>
2022-09-15 11:23:33 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.37646d71ac0580f29b5343ecc27a32b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f232570c992316a2ec229ea3afa90f",
+    "url": "https://brainaxe303.github.io/csb-rq2wjf/index.html"
+  },
+  {
+    "revision": "73efc8fdb8c44968aa81",
+    "url": "https://brainaxe303.github.io/csb-rq2wjf/static/js/2.d76898d8.chunk.js"
+  },
+  {
+    "revision": "a2236a415688be952e9e",
+    "url": "https://brainaxe303.github.io/csb-rq2wjf/static/js/main.953251ab.chunk.js"
+  },
+  {
+    "revision": "b3018a88d8a57cab2b9e",
+    "url": "https://brainaxe303.github.io/csb-rq2wjf/static/js/runtime-main.6ddaee1b.js"
+  }
+]);
